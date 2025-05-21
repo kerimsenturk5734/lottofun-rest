e4b8c1c4ad5dfb84f642931a3b10c27f0fda5e2a
Kerim Şentürk <kerimsenturk2002@outlook.com>
2025-05-21 22:32:53 +0300

    Initail commit

---
+++ src/main/java/com/lottofun/lottofunrest/LottofunRestApplication.java
@@ -0,0 +1,13 @@
+package com.lottofun.lottofunrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottofunRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottofunRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lottofun/lottofunrest/LottofunRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lottofun.lottofunrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LottofunRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
