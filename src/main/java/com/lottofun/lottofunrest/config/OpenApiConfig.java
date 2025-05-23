055590bea33fd1cb6b655a42d30eee2346a83b85
Kerim Şentürk <kerimsenturk2002@outlook.com>
2025-05-24 02:09:23 +0300

    fix(doc): swagger none working authorize field fixed

--- src/main/java/com/lottofun/lottofunrest/config/OpenApiConfig.java
+++ src/main/java/com/lottofun/lottofunrest/config/OpenApiConfig.java
@@ -1,25 +1,33 @@
 package com.lottofun.lottofunrest.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-        name = "Bearer Authentication",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "Bearer"
-)
 public class OpenApiConfig {
     @Bean
     public OpenAPI configureOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
-                .info(new Info().title("LottoFun")
+                .info(new Info()
+                        .title("LottoFun")
                         .description("LottFun Rest Docs")
-                        .version("1.0"));
+                        .version("1.0"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }
