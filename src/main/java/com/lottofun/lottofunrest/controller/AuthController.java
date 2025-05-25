54267691b82a5d9e3544874b6a43e7124c27e97f
Kerim Şentürk <kerimsenturk2002@outlook.com>
2025-05-25 19:19:28 +0300

    doc: register and login 201 response examples added

--- src/main/java/com/lottofun/lottofunrest/controller/AuthController.java
+++ src/main/java/com/lottofun/lottofunrest/controller/AuthController.java
@@ -6,6 +6,11 @@ import com.lottofun.lottofunrest.dto.request.RegisterRequest;
 import com.lottofun.lottofunrest.dto.response.LoginResponse;
 import com.lottofun.lottofunrest.dto.wrapper.ApiResult;
 import com.lottofun.lottofunrest.service.AuthService;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -26,6 +31,26 @@ public class AuthController {
         this.authService = authService;
     }
 
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "User login successful",
+                    content = @Content(
+                            schema = @Schema(implementation = ApiResult.class),
+                            examples = @ExampleObject(value = """
+                                        {
+                                          "status": 201
+                                          "success": success,
+                                          "message": "Insufficient balance",
+                                          "data": {
+                                                "token": "eyJhbGciOiJIUzI1NiI...",
+                                                "expireAt": "2021-12-31T23:59:59.999Z",
+                                          }
+                                        }
+                                    """)
+                    )
+            ),
+    })
     @PostMapping("/login")
     public ResponseEntity<ApiResult<LoginResponse>> login(@RequestBody @Valid LoginRequest loginRequest) {
         var status = HttpStatus.CREATED;
@@ -34,6 +59,30 @@ public class AuthController {
 
         return new ResponseEntity<>(ApiResult.success(message, data, status), status);
     }
+
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "User registered successfully",
+                    content = @Content(
+                            schema = @Schema(implementation = ApiResult.class),
+                            examples = @ExampleObject(value = """
+                                        {
+                                          "status": 201
+                                          "success": true,
+                                          "message": "Insufficient balance",
+                                          "data": {
+                                               "id": 1,
+                                               "name": "john",
+                                               "surname": "doe",
+                                               "username": "johndoe",
+                                               "balance": 100
+                                             }
+                                        }
+                                    """)
+                    )
+            ),
+    })
     @PostMapping("/register")
     public ResponseEntity<ApiResult<UserDto>> register(@RequestBody @Valid RegisterRequest registerRequest) {
         var status = HttpStatus.CREATED;
